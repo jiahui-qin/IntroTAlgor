9e8bab9fee5dcd8a67cf84dadc6d57730b4a3148
saber <feverqjh@outlook.com>
2017-12-13 22:36:06 +0800

    =

--- dynamic_programing/matrix_multi.py
+++ dynamic_programing/matrix_multi.py
@@ -8,7 +8,7 @@ def matrix_chain_order(p):
     s = [[0 for col in range(n)] for row in range(n)] #初始化两个n*n的矩阵
     for l in range(1, n):
         for i in range(0, n - l + 1):
-            j = i + l - 1
+            j = i + l 
             m[i][j] = float('inf')
             for k in range(i, j):
                 q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]
@@ -17,7 +17,7 @@ def matrix_chain_order(p):
                     s[i][j] = k
     return [m,s]
 chain = [30, 35, 15, 5, 10, 20, 25]
-[p,m]] = matrix_chain_order(chain)
+[p,m] = matrix_chain_order(chain)
 print(p)
 print(m)
 
